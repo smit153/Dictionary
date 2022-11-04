5bf523c64a346e67e0fe2c7ebe6f8dfa82bff1ac
smit153 <smitsojitra332003@gmail.com>
2022-11-04 22:34:21 +0530

    deploy

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ app.use("/", async (req, res, next) => {
 
   res.json({ message: all });
 });
+
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 const PORT = process.env.PORT || 8000;
 mongoose
   .connect(
