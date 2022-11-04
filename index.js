4b82a38b6eb4a387fffa0df535e8d5de8d814fcd
smit153 <smitsojitra332003@gmail.com>
2022-11-04 22:44:05 +0530

    Final changes

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+// app.use(express.static("client/build"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// });
 
 app.use("/add", (req, res, next) => {
   const word = new Word({
